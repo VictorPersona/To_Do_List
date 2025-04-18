bc92c2db6f6996e604af12c4a0647c27809c6b1a
VictorPersona <gauravtupe00@gmail.com>
2025-04-18 11:45:09 +0530

    add Tailwind CSS integration and implement TaskCard component for task management

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import TaskCard from './components/TaskCard'
 
 const App = () => {
+  const [task, setTask] = useState('')
+  const [taskArray, setTaskArray] = useState([])
+
+  const handleTaskChange = (taskValue) => {
+    setTask(taskValue)
+  }
+
+  const addTask = () => {
+    setTaskArray([
+      ...taskArray,
+      {
+        id: Date.now(),
+        title: task,
+        completed: false,
+      },
+    ])
+    console.log(taskArray)
+  }
+
+  const deleteTask = (taskId) => {
+    const updatedArray = taskArray.filter((prev) => prev.id !== taskId)
+    setTaskArray(updatedArray)
+  }
+
+  const onToggle = (taskId) => {
+    const updatedArray = taskArray.map((task)=>task.id ===taskId ? {...task,completed:!task.completed}:task)
+    setTaskArray(updatedArray)
+  }
+
   return (
-    <div>App</div>
+    <div className="  flex flex-col  gap-4 items-center bg-gray-100  border">
+      <h1>To Do List</h1>
+      <div className="">
+        <input
+          className="border "
+          onChange={(e) => handleTaskChange(e.target.value)}
+          type="text"
+          value={task}
+          placeholder="Enter Your Task"
+        />
+        <button
+          className="bg-green-600 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded-xl transition duration-200 shadow-md cursor-pointer"
+          onClick={() => addTask()}
+        >
+          Add
+        </button>
+      </div>
+      <div className="">
+        {taskArray.map((task) => (
+          <TaskCard key={task.id} task={task} onDelete={deleteTask} onToggle={onToggle} />
+        ))}
+      </div>
+    </div>
   )
 }
 
---
+++ client/src/components/TaskCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const TaskCard = ({ task, onDelete,onToggle }) => {
+  return (
+    <div className="flex gap-3 items-center">
+      <div onClick={()=>onToggle(task.id)} className={`cursor-pointer  w-full ${task.completed ? 'line-through text-gray-400':""}`  }>{task.title}</div>
+      
+      <button
+        onClick={() => onDelete(task.id)}
+        className="bg-red-500 text-white font-semibold px-4 py-2 rounded-xl cursor-pointer"
+      >
+       
+        Delete
+      </button>
+    </div>
+  )
+}
+
+export default TaskCard
